acb6a383e89691a498693fd1e1cf2b99887550bf
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-16 10:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 
+    'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 
+    'An Old Dog'
+];
+
+function sortArticles(bandArticle) {
+    let words = ['A ', 'An ', 'The '];
+    for(let word of words) {
+        let n = word.length;
+        if(bandArticle.startsWith(word)) {
+            return bandArticle.slice(n);
+        }
+    }
+    return bandArticle;
+}
+
+const sortedBands = bands.sort((a, b) => sortArticles(a).localeCompare(sortArticles(b)));
+
+const bandLi = document.getElementById('bands');
+
+sortedBands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    bandLi.appendChild(li);
+});
